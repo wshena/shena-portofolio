0c47fefb9a7147576103a0272ec7d557803761a3
wshena <wshenaarrafi@gmail.com>
2026-05-30 11:41:15 +0700

    feat:create about page, contact page, and projects page

---
+++ app/contact/page.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { IoMdSend } from "react-icons/io";
+
+const page = () => {
+  return (
+    <section className="container py-7.5 lg:py-0 xl:px-17.5 flex items-center lg:items-start lg:justify-between lg:gap-5 xl:gap-0">
+      <div className="h-screen w-screen lg:w-[50%] lg:flex items-center lg:justify-between">
+        <div className="flex flex-col gap-10">
+          <h1 className="text-3xl lg:text-4xl uppercase font-bold tracking-widest">
+            Contact Me
+          </h1>
+          <form action="" className="flex flex-col gap-10">
+            <div className="flex flex-col md:flex-row items-center gap-5">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="NAME"
+                autoComplete="off"
+                className="focus:outline-none pb-2.5 border-b w-full"
+              />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="YOUR EMAIL"
+                autoComplete="off"
+                className="focus:outline-none pb-2.5 border-b w-full"
+              />
+            </div>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              placeholder="SUBJECT"
+              autoComplete="off"
+              className="focus:outline-none pb-2.5 border-b"
+            />
+            <input
+              type="text"
+              name="project"
+              id="project"
+              placeholder="PROJECT"
+              autoComplete="off"
+              className="focus:outline-none pb-2.5 border-b"
+            />
+
+            <button className="py-2.5 px-5 gap-3.75 inline-flex w-fit items-center text-white bg-orange-500 hover:scale-[.9] transition-transform duration-300">
+              <IoMdSend />
+              <span className="uppercase tracking-widest">SEND EMAIL</span>
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div
+        className="hidden lg:block h-screen w-[50%] bg-cover bg-center"
+        style={{
+          backgroundImage: `url('/wave.jpg')`,
+        }}
+      ></div>
+    </section>
+  );
+};
+
+export default page;
---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Projects from "@/components/Projects";
+
+const page = () => {
+  return <Projects className="container xl:px-17.5 mb-12.5" />;
+};
+
+export default page;
